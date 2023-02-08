81aca1fc07f42f798bd8b6c47ce6e7f760b9ce0c
Katie Lonsdale <katie952@gmail.com>
2023-02-08 09:42:40 -0700

    test: add initialize tests

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require './lib/car'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  describe '#initialize' do
+    it 'exists' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership).to be_a Dealership
+    end
+
+    it 'has a name' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.name).to eq('Acme Auto')
+    end
+
+    it 'has and address' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.address).to eq('123 Main Street')
+    end
+
+    it 'has no inventory by default' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.inventory).to be_a Array 
+      expect(dealership.inventory.count).to be 0
+    end
+  end
+
+  # describe '#inventory_count' do
+  #   it 'returns number of cards in inventory' do
+
+  #   end
+  # end
+end
