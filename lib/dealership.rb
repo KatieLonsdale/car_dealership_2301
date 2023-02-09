7ea381685dda7d8a937619b7845342aa5716b7a1
Katie Lonsdale <katie952@gmail.com>
2023-02-09 13:43:23 -0700

    refactor

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -12,25 +12,26 @@ class Dealership
   end
 
   def add_car(car)
-    @inventory.push(car)
+    @inventory << car
   end
 
   def has_inventory?
-    self.inventory_count > 0 ? true : false
+    inventory_count > 0
   end
 
   def cars_by_make(make)
     @inventory.find_all do |car|
       car.make == make
     end
+    # @inventory.select do |car|
+      # car.make == make
+    # end
   end
 
   def total_value
-    total_value_of_inventory = 0
-    @inventory.each do |car|
-      total_value_of_inventory += car.total_cost
+    @inventory.sum do |car|
+      car.total_cost
     end
-    total_value_of_inventory
   end
 
   def details
@@ -41,40 +42,16 @@ class Dealership
   end
 
   def average_price_of_car
-    average_price = 0
-    @inventory.each do |car|
-      average_price += car.total_cost
-    end
-    average_price /= inventory_count
-    formatted_average_price = ''
     count = 0
-    average_price.to_s.reverse.each_char do |char|
-      if count == 3
-        formatted_average_price.prepend(',')
-        formatted_average_price.prepend(char)
-        count = 0
-      else 
-        formatted_average_price.prepend(char)
-        count += 1
-      end
-    end
-    formatted_average_price
+    (total_value / inventory_count).to_s.insert(2,',')
   end
-
+  
   def cars_sorted_by_price
     @inventory.sort_by{|car| car.total_cost}
   end
 
   def inventory_hash
-    dealership_inventory = {}
-    @inventory.each do |car|
-      if dealership_inventory.has_key?(car.make) == true
-        dealership_inventory[car.make].push(car)
-      else
-        dealership_inventory[car.make] = []
-        dealership_inventory[car.make].push(car)
-      end
-    end
+    dealership_inventory = @inventory.group_by{|car| car.make}
     dealership_inventory
   end
 end
